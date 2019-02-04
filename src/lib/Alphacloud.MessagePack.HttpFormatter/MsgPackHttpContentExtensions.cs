6bc27c9d97b9999d7e22017796fd6ea86b37e8ce
vk <vk@alphacloud.net>
2019-02-03 20:43:49 -0500

    Implement MediaTypeFormatter for Microsoft.AspNet.WebApi.Client
    Adds MsgPack support for HttpClient.
    
    closes #9

---
+++ src/lib/Alphacloud.MessagePack.HttpFormatter/MsgPackHttpContentExtensions.cs
@@ -0,0 +1,36 @@
+﻿namespace Alphacloud.MessagePack.HttpFormatter
+{
+    using System.Net.Http;
+    using System.Net.Http.Formatting;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using JetBrains.Annotations;
+    using WebApi.Client;
+
+
+    /// <summary>
+    ///     Extension methods to allow strongly typed objects to be read from <see cref="T:System.Net.Http.HttpContent" />
+    ///     instances using MsgPack format.
+    /// </summary>
+    [PublicAPI]
+    public static class MsgPackHttpContentExtensions
+    {
+        static readonly MediaTypeFormatter[] MsgpackOnlyFormatter =
+        {
+            MsgPackHttpClientExtensions.DefaultFormatter
+        };
+
+        /// <summary>
+        ///     Deserialize MsgPack response into specified type <typeparamref name="T" />.
+        /// </summary>
+        /// <remarks><see cref="MsgPackHttpClientExtensions.DefaultFormatter" /> is used to deserialize response.</remarks>
+        /// <typeparam name="T">The type of object to read.</typeparam>
+        /// <returns>A task object representing reading the content as an object of the specified type.</returns>
+        public static Task<T> ReadMsgPackAsAsync<T>(
+            this HttpContent content,
+            CancellationToken cancellationToken)
+        {
+            return content.ReadAsAsync<T>(MsgpackOnlyFormatter, cancellationToken);
+        }
+    }
+}
--- src/tests/Tests.Functional/Setup/TestServerSetup.cs
+++ src/tests/Tests.Functional/Setup/TestServerSetup.cs
@@ -2,8 +2,11 @@
 {
     using System;
     using System.Net.Http;
+    using System.Net.Http.Formatting;
     using System.Net.Http.Headers;
     using Alphacloud.MessagePack.AspNetCore.Formatters;
+    using Alphacloud.MessagePack.WebApi.Client;
+    using MessagePack.Resolvers;
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.AspNetCore.TestHost;
     using NetCoreWebApi;
@@ -11,6 +14,7 @@
 
     public class TestServerSetup : IDisposable
     {
+        public MediaTypeFormatterCollection Formatters { get; }
         public HttpClient Client { get; }
         public TestServer Server { get; }
 
@@ -24,6 +28,13 @@
             Client = Server.CreateClient();
             Client.BaseAddress = Server.BaseAddress;
             Client.DefaultRequestHeaders.Accept.Add(MediaTypeWithQualityHeaderValue.Parse(MessagePackFormatterOptions.DefaultContentType));
+
+            Formatters = new MediaTypeFormatterCollection(new MediaTypeFormatter[]
+            {
+                new MessagePackMediaTypeFormatter(ContractlessStandardResolver.Instance, new[] {MessagePackMediaTypeFormatter.DefaultMediaType}),
+                new BsonMediaTypeFormatter(),
+                new JsonMediaTypeFormatter()
+            });
         }
 
         /// <inheritdoc />
